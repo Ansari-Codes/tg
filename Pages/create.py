a1c8c85a6fba632b83b857dab102a327ea96ee2a
Ansari-Codes <ansaricodes@gmail.com>
2025-12-04 20:13:41 +0500

    .

---
+++ Pages/create.py
@@ -0,0 +1,21 @@
+from UI import Label, Header, Input, Button, TextArea, SoftBtn, Col, Row, AddSpace, Html, ui
+
+async def render():
+    with Header():
+        Label("Create").classes("text-lg font-bold")
+        AddSpace()
+        Button("Run")
+        Input()
+        Button("Save")
+    
+    with ui.splitter() as s:
+        with s.before:
+            with Col():
+                with ui.splitter() as sp:
+                    with sp.before:
+                        TextArea()
+                    with sp.after:
+                        TextArea()
+        with s.after:
+            Html('<canvas width="500px" height="500px" ></canvas>')
+
--- Routes.py
+++ Routes.py
@@ -1,6 +1,7 @@
 from Pages.welcome import render as rw
 from Pages.Auth.signup import render as rs
 from Pages.dashboard import render as rd
+from Pages.create import render as rc
 from ENV import ui
 
 @ui.page("/")
@@ -14,3 +15,7 @@ async def csp():
 @ui.page("/dashboard")
 async def cd():
     await rd()
+
+@ui.page("/create/{name}")
+async def cc(name: str):
+    await rc()
